585a24471acfee85f489b676752bd50a3d683e15
PazAutumn <pazcontrerastello@gmail.com>
2017-11-20 12:39:56 -0300

    primera cajita enrollement :')

--- js/app.js
+++ js/app.js
@@ -3,4 +3,12 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+console.log(data);	
+
+//enrollement
+var check = document.getElementById('scl2017-2');
+console.log(enrollement);
+var enrollement = document.getElementById('enrolled');
+	enrollement.textContent = data.SCL["2017-2"].students.length;
+
+//dropout
