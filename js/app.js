2f54798584a072e757e2c22db15e5546140808b7
PazAutumn <pazcontrerastello@gmail.com>
2017-11-21 00:48:01 -0300

    tech skills

--- js/app.js
+++ js/app.js
@@ -30,10 +30,7 @@ dropout.innerHTML = counter;
 
 //Achievement
 var achievement = document.getElementById('meetTarget');
-var ratings = data.SCL["2017-2"].ratings;
-var cumple = 0;
-var sprint1;
-var sprint2;
+
 /* formula para calcular el porcentaje de aprobación
 
 [Puntaje Técnico] = ([Técnico S1] + [Técnico S2] + [Técnico S3]) / [Cantidad Sprints]
@@ -48,11 +45,39 @@ hse=1200
 
 
 //porcentaje de alumnas que alcanzan el target
-	for (var i = 0; i < ratings.length; i++){
+	/*for (var i = 0; i < ratings.length; i++){
 	 if(ratings[i].student.cumple >= 70){
 	 	cumple = ratings[i].student.cumple;
 	 }
 }
+*/
+//NPS
 
+
+//Tech Skills
+var tech = document.getElementById('numTech');
+var ratings = data.SCL["2017-2"].ratings;
+//guardando en variables los datos de rating de las alumnas que cumplen con el 70%
+var techSprint1 = ratings[0].student.cumple;
+var techSprint2 = ratings[1].student.cumple;
+//cambiando el porcentaje a numero
+techSprint1 = parseInt((techSprint1*generation.length)/100);
+techSprint2 = parseInt((techSprint2*generation.length)/100);
+//sacando numero en promedio de estudiantes que superaron el 70% del tech en todos los sprints
+var techAverage = parseInt((techSprint1+techSprint2)/2);
+tech.innerHTML = techAverage;
+//porcentaje del tech
+var porTech = document.getElementById('porTech');
+
+var techPercent = parseInt((techAverage*100)/generation.length) + "%";
+porTech.innerHTML = techPercent;
+
+//Life Skills
+
+//STUDENT SATISFACTION
+
+//TEACHER RATING
+
+//JEDI MASTER RATING
 
 
